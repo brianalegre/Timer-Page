27ae2490103d4b833f3a7bb955746377eaf1398d
Brian Alegre <brialegre@yahoo.com>
2022-04-07 14:58:58 -0700

    Baseline code

---
+++ script.js
@@ -0,0 +1,17 @@
+// Selects element by class
+
+// Selects element by id
+
+  // Sets interval in variable
+
+
+// Function to create and append colorsplosion image
+function sendMessage() {
+  timeEl.textContent = " ";
+  var imgEl = document.createElement("img");
+  imgEl.setAttribute("src", "images/image_1.jpg");
+  mainEl.appendChild(imgEl);
+
+}
+
+setTime();
